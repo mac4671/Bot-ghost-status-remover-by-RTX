41ef2ebc22a34289ae95c94af840125c75c29342
mac4671 <mahmoudkhalifagn@gmail.com>
2024-10-20 15:53:29 -0600

    MAIN PLS index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["#1 growing design server", "Watching over Artic Designs"];
+const statusTypes = [ 'idle', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
